a232329c2c6707c04210c7c67f68b8da405b09da
gerthermans <gerthermans747@gmail.com>
2017-10-07 16:09:52 +0200

    some work on authentication

--- KwakkelbergBookShare/KwakkelbergBookShare/Startup.cs
+++ KwakkelbergBookShare/KwakkelbergBookShare/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using KwakkelbergBookShare.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(KwakkelbergBookShare.Startup))]
@@ -9,6 +12,39 @@ namespace KwakkelbergBookShare
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesAndUsers();
+        }
+
+        private void createRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "gerthermans747@gmail.com";
+                user.Email = "gerthermans747@gmail.com";
+                string userPWD = "Administrator747";
+                var chkUser = userManager.Create(user, userPWD);
+                if (chkUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            if(!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+            }
         }
     }
+
+
 }
